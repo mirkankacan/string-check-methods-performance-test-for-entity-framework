b380731113ab06c0b8fa188caae0740a341f6eec
Mirkan Kaçan <kacanmirkan@gmail.com>
2023-12-21 19:52:52 +0300

    Performance Test

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using PerformanceTest.Models;
+using System;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace PerformanceTest
+{
+    public class Program
+    {
+        private async void Main(string[] args)
+        {
+            string str = "test string";
+            Stopwatch s = new Stopwatch();
+
+            s.Start();
+            using (TestEntites db = new TestEntites())
+            {
+                bool xx = await db.TestTable.AnyAsync(x => x.TestColumm.Contains(str));
+            }
+            s.Stop();
+            Console.WriteLine("{0}ms using Contains", s.Elapsed.TotalMilliseconds);
+
+            s.Reset();
+
+            s.Start();
+            using (TestEntites db = new TestEntites())
+            {
+                bool xx = await db.TestTable.AnyAsync(x => x.TestColumm.StartsWith(str);
+            }
+            s.Stop();
+            Console.WriteLine("{0}ms using StartsWith", s.Elapsed.TotalMilliseconds);
+
+            s.Reset();
+
+            s.Start();
+            using (TestEntites db = new TestEntites())
+            {
+                bool xx = await db.TestTable.AnyAsync(x => x.TestColumm.Equals(str);
+            }
+            s.Stop();
+            Console.WriteLine("{0}ms using Equals", s.Elapsed.TotalMilliseconds);
+
+            s.Reset();
+
+            s.Start();
+            using (TestEntites db = new TestEntites())
+            {
+                bool xx = await db.TestTable.AnyAsync(x => x.TestColumm == str);
+            }
+            s.Stop();
+            Console.WriteLine("{0}ms using ==", s.Elapsed.TotalMilliseconds);
+
+            Console.WriteLine("Press a key to quit...");
+            Console.ReadKey();
+        }
+    }
+}
